efa1720c7fd2dd58493af2fe4a1cd5fc4f9ceab7
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-06-23 18:48:55 +0300

    page_update

---
+++ page/ProfileCard/script.js
@@ -0,0 +1,9 @@
+let destroy_icon = document.querySelector(".bom");
+let destroy_span = document.querySelector("#destroy");
+
+function destroy(){
+    destroy_span.innerHTML = "";
+}
+
+addEventListener("click", destroy);
+
